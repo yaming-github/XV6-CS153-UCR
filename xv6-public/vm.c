b7a3fe453f00a02b8d6a636d5a4158e27857178b
Yaming Zhang <yzhan737@sledge.cs.ucr.edu>
2020-02-20 15:26:24 -0800

    remove some annotations

--- xv6-public/vm.c
+++ xv6-public/vm.c
@@ -315,36 +315,33 @@ clearpteu(pde_t *pgdir, char *uva)
 pde_t*
 copyuvm(pde_t *pgdir, uint sz)
 {
-//  pde_t *d;
+  pde_t *d;
   pte_t *pte;
-//  uint pa, i, flags;
-  uint i;
-//  char *mem;
+  uint pa, i, flags;
+  char *mem;
 
-//  if((d = setupkvm()) == 0)
-//    return 0;
+  if((d = setupkvm()) == 0)
+    return 0;
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
       panic("copyuvm: pte should exist");
     if(!(*pte & PTE_P))
       panic("copyuvm: page not present");
-    PTE_W = 0;
-//    pa = PTE_ADDR(*pte);
-//    flags = PTE_FLAGS(*pte);
-//    if((mem = kalloc()) == 0)
-//      goto bad;
-//    memmove(mem, (char*)P2V(pa), PGSIZE);
-//    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
-//      kfree(mem);
-//      goto bad;
-//    }
+    pa = PTE_ADDR(*pte);
+    flags = PTE_FLAGS(*pte);
+    if((mem = kalloc()) == 0)
+      goto bad;
+    memmove(mem, (char*)P2V(pa), PGSIZE);
+    if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+      kfree(mem);
+      goto bad;
     }
-    return pgdir;
-//  return d;
+  }
+  return d;
 
-//bad:
-//  freevm(d);
-//  return 0;
+bad:
+  freevm(d);
+  return 0;
 }
 
 //PAGEBREAK!
